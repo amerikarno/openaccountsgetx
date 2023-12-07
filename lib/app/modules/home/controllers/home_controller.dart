71e8e9c4975427a37ef5c601f1e47cdb7e036c61
Karnake Ruengchaicharnkij <karnake.r@finansiada.com>
2023-12-07 16:48:07 +0700

    preinfo page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,26 +1,148 @@
+import 'dart:developer';
+
+import 'package:email_validator/email_validator.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   final count = 0.obs;
-  final thTitle = "".obs;
+  String? thTitle;
+  String? thTitleErrorMessage;
+  final thList = [
+    'นาย',
+    'นาง',
+    'นางสาว',
+  ];
+  String? engTitle;
+  String? engTitleErrorMessage;
+  final engList = [
+    'Mr.',
+    'Mrs.',
+    'Ms.',
+  ];
 
-  @override
-  void onInit() {
-    super.onInit();
+  String? thName;
+  String? thNameErrorMessage;
+  String? thSurnameErrorMessage;
+  String? thSurname;
+  String? engName;
+  String? engSurname;
+  String? engNameErrorMessage;
+  String? engSurnameErrorMessage;
+  String? email;
+  String? emailErrorMessage;
+  String? mobile;
+  String? mobileErrorMessage;
+  bool? agreement = false;
+
+  String? engToTh(String? eng) {
+    if (eng == null) return null;
+    final index = engList.indexOf(eng);
+    if (index >= 0) {
+      log('eng: ${thList[index]}');
+      return thList[index];
+    }
+    return null;
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
+  String? thToEng(String? th) {
+    if (th == null) return null;
+    final index = thList.indexOf(th);
+    if (index >= 0) {
+      log('th: ${engList[index]}');
+      return engList[index];
+    }
+    return null;
   }
 
   void increment() => count.value++;
-  void setThTitleName(value) => thTitle.value = value;
+  void setThTitleName(value) {
+    if (thTitle == null) {
+      thTitleErrorMessage = 'กรุณาใส่คำนำหน้าชื่อ (ภาษาไทย)';
+    }
+    thTitle = value;
+    engTitle = thToEng(value)!;
+    update();
+  }
+
+  void setThName(value) {
+    if (value.isBlank) {
+      thNameErrorMessage = 'กรุณาใส่ชื่อ (ภาษาไทย)';
+    }
+    thName = value;
+    update();
+  }
+
+  void setThSurname(value) {
+    if (value.isBlank) {
+      thSurnameErrorMessage = 'กรุณาใส่นามสกุล (ภาษาไทย)';
+    }
+    thSurname = value;
+    update();
+  }
+
+  void setEngName(value) {
+    if (value.isBlank) {
+      engNameErrorMessage = 'กรุณาใส่ชื่อ (ภาษาอังกฤษ)';
+    }
+    engName = value;
+    update();
+  }
+
+  void setEngSurname(value) {
+    if (value.isBlank) {
+      engSurnameErrorMessage = 'กรุณาใส่นามสกุล (ภาษาอังกฤษ)';
+    }
+    engSurname = value;
+    update();
+  }
+
+  void setEngTitleName(value) {
+    if (engTitle == null) {
+      engTitleErrorMessage = 'กรุณาใส่คำนำหน้าชื่อ (ภาษาอังกฤษ)';
+    }
+    engTitle = value;
+    thTitle = engToTh(value)!;
+    update();
+  }
+
+  void verifyEmailFormat(value) {
+    if (engTitle == null) {
+      emailErrorMessage = 'กรุณาใส่อีเมล';
+    }
+    if (!EmailValidator.validate(value)) {
+      emailErrorMessage = 'กรุณาใส่อีเมลให้ถูกต้อง';
+    }
+    email = value;
+    update();
+  }
+
+  void verifyMobileFormat(value) {
+    if (engTitle == null) {
+      mobileErrorMessage = 'กรุณาใส่หมายเลขโทรศัพท์มือถือ';
+    }
+    if (value.startsWith('06') ||
+        value.startsWith('08') ||
+        value.startsWith('09')) {
+      if (value.length != 10) {
+        mobileErrorMessage = 'กรุณาใส่หมายเลขโทรศัพท์มือถือให้ถูกต้อง';
+      }
+    } else {
+      if (value.length != 9) {
+        mobileErrorMessage = 'กรุณาใส่หมายเลขโทรศัพท์มือถือให้ถูกต้อง';
+      }
+    }
+    engTitle = value;
+    update();
+  }
+
+  void checkAgreement(value) {
+    agreement = value;
+    update();
+  }
+  void popupAgreement() {
+    agreement = true;
+    update();
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,42 +1,251 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:openaccountsgetx/app/data/preInfo_data.dart';
+import 'package:openaccountsgetx/app/data/preinfo_data.dart';
 
 import '../controllers/home_controller.dart';
 
-class HomeView extends GetView<HomeController> {
+class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    final width = Get.size.width;
+    // String? errorEmailMessage;
+    // final email = false.obs;
+
     return Scaffold(
       appBar: AppBar(
         title: AutoSizeText(width * .3, 'กรุณากรอกข้อมูลเพื่อเปิดบัญชี'),
         centerTitle: true,
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(width * 0.2, 0, width * 0.2, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Obx(() => DropdownButtonFormField(
-                  // value: controller.thTitle.value,
-                  onChanged: (value) => controller.setThTitleName(value),
-                  items: thList.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                        value: value, child: Text(value));
-                  }).toList(),
+      body: GetBuilder<HomeController>(
+        builder: (ctrl) {
+          final thTitleDropDown = DropdownButtonFormField(
+              decoration: InputDecoration(
+                  label: FittedBox(
+                child: RichText(
+                    text: const TextSpan(
+                        text: 'คำนำหน้าชื่อ (ภาษาไทย)',
+                        children: [
+                      TextSpan(
+                          text: '*', style: TextStyle(color: Colors.orange))
+                    ])),
+              )),
+              value: ctrl.thTitle,
+              onChanged: (value) => ctrl.setThTitleName(value),
+              items: [
+                for (var data in ctrl.thList)
+                  DropdownMenuItem(
+                    value: data,
+                    child: Text(data),
+                  )
+              ]);
+
+          final thNameTextField = TextField(
+            decoration: InputDecoration(
+                label: FittedBox(
+                  child: RichText(
+                      text: const TextSpan(text: 'ชื่อ (ภาษาไทย)', children: [
+                    TextSpan(text: '*', style: TextStyle(color: Colors.red))
+                  ])),
                 ),
-              )
-              // Text(
-              //   'HomeView is working',
-              //   style: TextStyle(fontSize: 20),
-              // ),
+                errorText: ctrl.thNameErrorMessage),
+            onChanged: (value) => ctrl.setThName(value),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[ก-๛ ]'))
             ],
-          ),
-        ),
+          );
+
+          final thSurnameTextField = TextField(
+            decoration: InputDecoration(
+                label: FittedBox(
+                    child: RichText(
+                        text: const TextSpan(
+                            text: 'นามสกุล (ภาษาไทย)',
+                            children: [
+                      TextSpan(text: '*', style: TextStyle(color: Colors.red))
+                    ]))),
+                errorText: ctrl.thSurnameErrorMessage),
+            onChanged: (value) => ctrl.setThSurname(value),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[ก-๛ ]'))
+            ],
+          );
+
+          final engTitleDropDown = DropdownButtonFormField(
+              decoration: InputDecoration(
+                  label: FittedBox(
+                child: RichText(
+                    text: const TextSpan(
+                        text: 'คำนำหน้าชื่อ (ภาษาไทย)',
+                        children: [
+                      TextSpan(
+                          text: '*', style: TextStyle(color: Colors.orange))
+                    ])),
+              )),
+              value: ctrl.engTitle,
+              onChanged: (value) => ctrl.setEngTitleName(value),
+              items: [
+                for (var data in ctrl.engList)
+                  DropdownMenuItem(
+                    value: data,
+                    child: Text(data),
+                  )
+              ]);
+
+          final engNameTextField = TextField(
+            decoration: InputDecoration(
+                label: FittedBox(
+                  child: RichText(
+                      text: const TextSpan(
+                          text: 'ชื่อ (ภาษาอังกฤษ)',
+                          children: [
+                        TextSpan(text: '*', style: TextStyle(color: Colors.red))
+                      ])),
+                ),
+                errorText: ctrl.engNameErrorMessage),
+            onChanged: (value) => ctrl.setEngName(value),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z ]'))
+            ],
+          );
+
+          final engSurnameTextField = TextField(
+            decoration: InputDecoration(
+                label: FittedBox(
+                    child: RichText(
+                        text: const TextSpan(
+                            text: 'นามสกุล (ภาษาอังกฤษ)',
+                            children: [
+                      TextSpan(text: '*', style: TextStyle(color: Colors.red))
+                    ]))),
+                errorText: ctrl.engSurnameErrorMessage),
+            onChanged: (value) => ctrl.setEngSurname(value),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z ]'))
+            ],
+          );
+          final emailTextField = TextField(
+            decoration: InputDecoration(
+                prefix: const Icon(Icons.email),
+                label: FittedBox(
+                    child: RichText(
+                        text: const TextSpan(text: 'อีเมล', children: [
+                  TextSpan(text: '*', style: TextStyle(color: Colors.red))
+                ]))),
+                errorText: ctrl.emailErrorMessage),
+            onChanged: (value) => ctrl.verifyEmailFormat(value),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9@.]'))
+            ],
+          );
+          final mobileTextField = TextField(
+            decoration: InputDecoration(
+                prefix: const Icon(Icons.phone_iphone),
+                label: FittedBox(
+                    child: RichText(
+                        text: const TextSpan(
+                            text: 'หมายเลขโทรศัพท์มือถือ',
+                            children: [
+                      TextSpan(text: '*', style: TextStyle(color: Colors.red))
+                    ]))),
+                errorText: ctrl.mobileErrorMessage),
+            onChanged: (value) => ctrl.verifyMobileFormat(value),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9@.]'))
+            ],
+          );
+
+          final agreement = CheckboxListTile(
+              overlayColor: MaterialStateProperty.all(Colors.transparent),
+              contentPadding: EdgeInsets.zero,
+              checkColor: Colors.white,
+              activeColor: Colors.green,
+              side: const BorderSide(color: Colors.grey),
+              title: const Text(
+                  'ข้าพเจ้าได้อ่านและตกลงตามข้อมกำหนดและเงื่อนไขและรับทราบนโยบายความเป็นส่วนตัว ซึ่งระบุวิธีการที่บริษัท ฟินันเซีย ดิจิตทัล แอสแซท จำกัด("บริษัท")',
+                  maxLines: 2),
+              controlAffinity: ListTileControlAffinity.leading,
+              value: ctrl.agreement,
+              onChanged: (value) => ctrl.checkAgreement(value));
+
+          final policyAgreement = TextButton(
+            style: ButtonStyle(
+              textStyle: MaterialStateProperty.all(const TextStyle(color: Colors.grey)),
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              overlayColor: MaterialStateProperty.all(Colors.transparent)
+            ),
+              onPressed: () => showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                        title: const Text('นโยบายความเป็นส่วนตัว'),
+                        content: SizedBox(
+                          width: (MediaQuery.of(context).size.width * 0.6),
+                          child: SingleChildScrollView(
+                            padding: const EdgeInsetsDirectional.all(15),
+                            child: Text(policyAgreementText),
+                          ),
+                        ),
+                        actionsAlignment: MainAxisAlignment.center,
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                ctrl.popupAgreement();
+                                Navigator.pop(context, 'OK');
+                              },
+                              child: const Text('ตกลง'))
+                        ],
+                      )),
+              child: const Text('อ่านรายละเอียดเพิ่มเติม', maxLines: 1,));
+
+         final usernamePasswordSubject = FittedBox(
+                        child: RichText(
+                            text: const TextSpan(
+                                text:
+                                    'ข้อมูลสำหรับรับ Username, Password และเอกสารจากทางบริษัทฯ',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold))),
+                      );
+
+          return Center(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.fromLTRB(width * 0.2, 0, width * 0.2, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: width * .2, child: thTitleDropDown),
+                  Row(
+                    children: [
+                      SizedBox(width: width * .2, child: thNameTextField),
+                      SizedBox(width: width * .01),
+                      SizedBox(width: width * .39, child: thSurnameTextField),
+                    ],
+                  ),
+                  SizedBox(width: width * .2, child: engTitleDropDown),
+                  Row(
+                    children: [
+                      SizedBox(width: width * .2, child: engNameTextField),
+                      SizedBox(width: width * .01),
+                      SizedBox(width: width * .39, child: engSurnameTextField),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                      width: width*.6,
+                      child: usernamePasswordSubject),
+                  // const SizedBox(height: 20),
+                  SizedBox(width: width * .6, child: emailTextField),
+                  SizedBox(width: width * .6, child: mobileTextField),
+                  SizedBox(width: width * .6, child: agreement),
+                  SizedBox(width: width * .2, child: policyAgreement)
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
